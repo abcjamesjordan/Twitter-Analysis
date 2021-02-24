da46d05707ef714bf730a2b08f09af9b515bf79e
James Jordan <30762515+abcjamesjordan@users.noreply.github.com>
2021-02-23 18:33:40 -0700

    first

---
+++ analysis.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import re
+
+path = 'C:\\Users\\abcja\\Documents\\Coding\\Data\\Twitter Sentiment\\training.1600000.processed.noemoticon.csv'
+df = pd.read_csv(path)
+
+cols = [1,2,3,4]
+df.drop(df.columns[cols], axis=1, inplace=True)
+
+txt = "The rain in Spain"
+x = re.search("^The.*Spain$", txt)
+print(x)
